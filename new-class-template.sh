1805077e9260240193dda1b6955d616c92865148
MisterDriver <mauricedriver347@gmail.com>
2025-04-11 18:54:47 -0400

    Update new-class-template.sh

--- new-class-template.sh
+++ new-class-template.sh
@@ -57,8 +57,8 @@ cat <<EOF > /var/www/html/index.html
     <div class="w3-display-topleft w3-padding-large w3-xlarge"></div>
     <div class="w3-display-middle w3-center">
       <img src="https://i.pinimg.com/736x/e9/90/25/e990256687be9b08b96cfd9162980367.jpg"
-              width="150"
-              height="200"
+              width="400"
+              height="550"
               style="border-radius:10px;"
               frameBorder="0"
               class="giphy-embed"
